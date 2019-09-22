e1d7ca315e57bbe91d371f36fbc9331f7a95b9a3
= <anthonybhasin@gmail.com>
2019-09-22 18:24:03 -0400

    Completed linking of first screens for our sprint 1

--- Android/app/src/main/java/edu/buffalo/cse/cse442f19/spotme/MainActivity.kt
+++ Android/app/src/main/java/edu/buffalo/cse/cse442f19/spotme/MainActivity.kt
@@ -1,13 +1,18 @@
 package edu.buffalo.cse.cse442f19.spotme
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        button.setOnClickListener {
+            val intent = Intent(this, MatchListActivity :: class.java)
+            startActivity(intent)
+        }
     }
 }
