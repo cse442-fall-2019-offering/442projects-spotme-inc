d265d78d427a235cdbac5dd24de5f3edba5e4c17
oscar <oscarmej@buffalo.edu>
2019-10-18 15:08:32 -0400

    Added my profile screen (See #9)

--- Android/app/src/main/java/edu/buffalo/cse/cse442f19/spotme/MatchProfileActivity.kt
+++ Android/app/src/main/java/edu/buffalo/cse/cse442f19/spotme/MatchProfileActivity.kt
@@ -4,9 +4,11 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MatchProfileActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_match_profile)
