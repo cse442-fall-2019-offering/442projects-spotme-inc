6eb115e2587932152766e3e88c7626dc0b9c01d3
montanaq <montana@buffalo.edu>
2019-10-20 14:47:03 -0400

    Started displaying user information on profile page.  Image and name are done.

--- Android/app/src/main/java/edu/buffalo/cse/cse442f19/spotme/MyProfile.kt
+++ Android/app/src/main/java/edu/buffalo/cse/cse442f19/spotme/MyProfile.kt
@@ -5,12 +5,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.TextView
 
 class MyProfile : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_profile)
+        val textView = findViewById(R.id.textView25) as TextView
+        textView.text = Globals.currentUser?.name
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
